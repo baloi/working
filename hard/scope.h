eed4bd7e55bf707b8a61062b782d29c7d4d765c9
baloi <baloi@drogo.my.domain>
2015-03-09 23:09:06 -0400

    hard

---
+++ hard/scope.h
@@ -0,0 +1,13 @@
+#ifndef __scope_h__
+#define __scope_h__
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// makes THE_SIZE in scope.c available to other .c files
+extern int THE_SIZE;
+
+// gets and sets an internal static variable in 
+
+#endif
